2143e209fa8e73f48578db17a4a1960c5ca290f7
Waleed Tariq <dev.ratech@outlook.com>
2022-11-02 02:40:09 +0500

    add theme in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
 function App() {
+  const [theme, colorMode] = useMode()
+
   return (
-    <div className="app">
-      
-    </div>
-  );
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
 
-export default App;
+export default App

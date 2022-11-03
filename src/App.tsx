09d79608bde53f9d62afbb9c6573b9ec7bda5fcc
Waleed Tariq <dev.ratech@outlook.com>
2022-11-04 01:22:58 +0500

    add dashboard, team, contacts and invoices routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { Routes, Route } from 'react-router-dom'
 import Topbar from './scenes/global/Topbar'
-import Dashboard from './scenes/dashboard'
 import Sidebar from './scenes/global/Sidebar'
+import Dashboard from './scenes/dashboard'
+import Team from './scenes/team'
+import Contacts from './scenes/contacts'
+import Invoices from './scenes/invoices'
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -19,6 +22,9 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
             </Routes>
           </main>
         </div>

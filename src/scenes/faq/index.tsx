3e23f456cd54fb6ef0de1e6eb30c6fe45f14ec67
Waleed Tariq <dev.ratech@outlook.com>
2022-11-05 03:18:09 +0500

    add faq page

---
+++ src/scenes/faq/index.tsx
@@ -0,0 +1,35 @@
+import { Box, useTheme, Typography } from '@mui/material'
+import Header from '../../components/Header'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui//material/AccordionDetails'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { tokens } from '../../theme'
+
+const FAQ = () => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subTitle="Frequently Asked Questions Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias
+            porro ut necessitatibus consequuntur quam quibusdam optio doloribus
+            incidunt, minima et dolore quod corrupti molestias? Ipsum quis a
+            unde ipsa. Est.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  )
+}
+
+export default FAQ
